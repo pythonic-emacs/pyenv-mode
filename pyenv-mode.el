bafa3597453b70b5e7305addcc3dd1097ce7c1e6
Malyshev Artem <proofit404@gmail.com>
2014-11-10 23:22:43 +0300

    Mention README in comments.

--- pyenv-mode.el
+++ pyenv-mode.el
@@ -21,22 +21,7 @@
 
 ;;; Commentary:
 
-;; Add following block to your Emacs configuration
-;;
-;;     (pyenv-mode)
-;;
-;; Now you are available to specify pyenv python installation
-;;
-;;     M-x pyenv-mode-set
-;;
-;; So now when you run inferior python with
-;;
-;;     M-x run-python
-;;
-;; process will start inside specified python installation.  You can
-;; unset current version with
-;;
-;;     M-x pyenv-mode-unset
+;; See the README for more details.
 
 ;;; Code:
 
