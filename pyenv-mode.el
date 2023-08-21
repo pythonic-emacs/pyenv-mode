6820aa6673e6a51ace88611a58b423b5b1effb19
Jen-Chieh Shen <jcs090218@gmail.com>
2023-08-21 09:45:00 -0700

    fix: Emacs as dependency (#39)

--- pyenv-mode.el
+++ pyenv-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Artem Malyshev <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/pyenv-mode
 ;; Version: 0.1.0
-;; Package-Requires: ((pythonic "0.1.0"))
+;; Package-Requires: ((emacs "25.1") (pythonic "0.1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
